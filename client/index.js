3cadfa3431264671c09cf1bfc44b5f8b8e878cb7
Jon Edwards <jkedwards@me.com>
2015-04-07 21:54:06 -0700

    adding operators and additional functionality

--- client/index.js
+++ client/index.js
@@ -1,30 +1,57 @@
 'use strict';
 
 var displayBuffer = '0';
+var operator = '';
+var firstNumber = 0;
+var secondNumber = 0;
 
 $(document).ready(function() {
 
 });
 
 $(document).on('keypress', function(keyCharCode) {
-	keyCharCode = String.fromCharCode(keyCharCode.which).toLowerCase();
+	keyCharCode = String.fromCharCode(keyCharCode.which).toUpperCase();
+  var liHit = $('p:contains("' + keyCharCode + '")').closest('li');
+  buttonHit(liHit);
   inputHandler(keyCharCode);
 });
 
 $('li').click(function() {
-  $(this).css('background-color', 'gray').delay('fast').
-  inputHandler($(this).find('p').text());
+  var valueOfClickedButton = $(this).find('p').text();
+  buttonHit(this);
+  inputHandler(valueOfClickedButton);
 });
 
+function buttonHit(button) {
+  var originalColor = $(button).css('background-color');
+  var newColor = ($(button).hasClass('orangebutton')) ? 'rgba(218, 146, 50, 0.75)' : 'rgba(227, 227, 227, 0.75)';
+  $(button).css('background-color', newColor).delay(100).queue(function() {
+    $(button).css('background-color', originalColor);
+  });
+}
+
 function inputHandler(char) {
-  console.log($(this).parent('li'));
   if(char.match(/\d/) !== null || char === '.') {
     putNumberInBufferAndDisplay(char);
   }
+  else if(char === 13) {
+    performOperation();
+  }
   switch (char) {
-    case 'c':
+    case 'C':
       clearDisplay();
       break;
+    case '+':
+      setOperation(char);
+      break;
+    case '-':
+      setOperation(char);
+      break;
+    case '=':
+      performOperation();
+      break;
+    case '*':
+
   }
 }
 
@@ -44,6 +71,16 @@ function putNumberInBufferAndDisplay(number) {
 }
 
 function clearDisplay() {
-  displayBuffer = 0;
+  displayBuffer = '0';
   $('#displaytext').text(displayBuffer);
+}
+
+function setOperation(oper) {
+  firstNumber = parseFloat(displayBuffer);
+  operator = oper;
+  clearDisplay();
+}
+
+function performOperation() {
+
 }
