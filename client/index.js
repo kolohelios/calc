d518f4f4a3f248d3fabd1dbf173a095e7dae4873
Jon Edwards <jkedwards@me.com>
2015-04-07 21:08:33 -0700

    got buffer handling set, including decimal

--- client/index.js
+++ client/index.js
@@ -1,14 +1,49 @@
 'use strict';
 
+var displayBuffer = '0';
+
 $(document).ready(function() {
 
 });
 
 $(document).on('keypress', function(keyCharCode) {
-	var char = String.fromCharCode(keyCharCode.which).toLowerCase();
-  console.log(char);
+	keyCharCode = String.fromCharCode(keyCharCode.which).toLowerCase();
+  inputHandler(keyCharCode);
 });
 
 $('li').click(function() {
-  console.log($(this).find('p').text());
+  $(this).css('background-color', 'gray').delay('fast').
+  inputHandler($(this).find('p').text());
 });
+
+function inputHandler(char) {
+  console.log($(this).parent('li'));
+  if(char.match(/\d/) !== null || char === '.') {
+    putNumberInBufferAndDisplay(char);
+  }
+  switch (char) {
+    case 'c':
+      clearDisplay();
+      break;
+  }
+}
+
+function putNumberInBufferAndDisplay(number) {
+  if(displayBuffer === '0') {
+    displayBuffer = number;
+  }
+  else if(number === '.') {
+    if(displayBuffer.search(/[.]/) <= 0) {
+      displayBuffer = displayBuffer.concat(number);
+    }
+  }
+  else {
+    displayBuffer = displayBuffer.concat(number);
+  }
+  $('#displaytext').text(displayBuffer);
+}
+
+function clearDisplay() {
+  displayBuffer = 0;
+  $('#displaytext').text(displayBuffer);
+}
