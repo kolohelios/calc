860e98b90dc3951e6e1730dabc244fdc60262cac
Jon Edwards <jkedwards@me.com>
2015-04-07 19:31:08 -0700

    finished easy ui and added ux basics

--- client/index.js
+++ client/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+$(document).ready(function() {
+
+});
+
+$(document).on('keypress', function(keyCharCode) {
+	var char = String.fromCharCode(keyCharCode.which).toLowerCase();
+  console.log(char);
+});
+
+$('li').click(function() {
+  console.log($(this).find('p').text());
+});
